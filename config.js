98981d63e123921b7e59845db3fb79f143988ea0
Jeff Patterson <jeff@tilikum.net>
2019-07-25 20:31:36 -0700

    replaces gatsby starter

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteTitle: 'Jeff Patterson', // <title>
+  manifestName: 'Eventually',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  //pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Jeff Patterson',
+  subHeading:
+    "Engineer focused on cultivating all the things that make delivering high quality software inevitable. Software is made of people.",
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/ciridium',
+    },
+  ],
+};
